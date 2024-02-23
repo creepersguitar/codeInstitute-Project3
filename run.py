a75920f51ca4cb7a051dc5c288d149da022ec4de
George Small <gsrr201@gmail.com>
2024-02-23 10:41:18 +0000

    changed num_ships=0 to num_ships=None

--- run.py
+++ run.py
@@ -191,7 +191,7 @@ class BattleshipGame:
 def welcome_prompt():
     """ Welcome message and allows inputs for board size and number of ships"""
     print("Welcome to battleships!")
-    num_ships = 0
+    num_ships = None
     while True:
         try:
             board_size = int(input("Enter the board size (default is 8): \n") or 8)
