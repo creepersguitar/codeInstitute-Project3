12ba26868037488058db06a72b22f5e46f4358ed
George Small <gsrr201@gmail.com>
2024-02-24 11:39:36 +0000

    readded place_ships to help with last commit issue

--- run.py
+++ run.py
@@ -57,29 +57,25 @@ class BattleshipGame:
             print("Error loading scoreboard", e)
 
     def place_ships(self):
-        """Places the ships randomly on the board."""
+        """ Helps to place the ships in random spots on the board """
         for _ in range(self.num_ships):
             length = random.randint(2, 5)
             horizontal = random.choice([True, False])
-            ship_placed = False
-
-            while not ship_placed:
+            try:
                 if horizontal:
                     row = random.randint(0, self.board_size - 1)
-                    col = random.randint(0, self.board_size - length)
-                    if all(self.board[row][c] == 'o' for c in range(col, col + length)):
-                        for i in range(length):
-                            self.board[row][col + i] = 'S'
-                        ship_placed = True
+                    col = random.randint(0, self.board_size - length)  # Adjusted range
+                    for i in range(length):
+                        self.board[row][col + i] = 'S'
                 else:
-                    row = random.randint(0, self.board_size - length)
+                    row = random.randint(0, self.board_size - length)  
                     col = random.randint(0, self.board_size - 1)
-                    if all(self.board[r][col] == 'o' for r in range(row, row + length)):
-                        for i in range(length):
-                            self.board[row + i][col] = 'S'
-                        ship_placed = True
-
-            self.ships.append((row, col, length, horizontal))
+                    for i in range(length):
+                        self.board[row + i][col] = 'S'
+                self.ships.append((row, col, length, horizontal))
+            except ValueError:
+                # If randrange() fails due to invalid range, retry placing the ship
+                self.place_ships()
 
 
     def print_board(self, board, show_ships=False):
